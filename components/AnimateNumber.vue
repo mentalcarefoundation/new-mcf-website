2dd8537ee80c31282fd57ea49f51a593f9bc17ee
khairahscorner <airahyusuff@gmail.com>
2021-10-11 07:27:48 +0100

    animation updates: to delay until scroll

--- components/AnimateNumber.vue
+++ components/AnimateNumber.vue
@@ -13,26 +13,32 @@ export default {
     data() {
         return {
             animatedVal: 0,
-            interval: false
+            interval: false,
+            observer: null
         }
     },
-    ready () {
-        this.animatedVal = this.number ? this.number : 0;
-    },
-
     mounted() {
-      clearInterval(this.interval);
-      if(this.number === this.animatedVal) {
-        return;
-      }
-      this.interval = window.setInterval(() => {
-        if(this.animatedVal !== this.number) {
-          let change = (this.number - this.animatedVal) / 10;
-          change = change >= 0 ? Math.ceil(change) : Math.floor(change);
-          this.animatedVal = this.animatedVal + change;
+      this.observer = new IntersectionObserver(entries => {
+        const item = entries[0];
+          if (item.isIntersecting) {
+            clearInterval(this.interval);
+            if(this.number === this.animatedVal) {
+              return;
+            }
+            this.interval = window.setInterval(() => {
+              if(this.animatedVal !== this.number) {
+                let change = (this.number - this.animatedVal) / 10;
+                change = change >= 0 ? Math.ceil(change) : Math.floor(change);
+                this.animatedVal = this.animatedVal + change;
+              }
+            }, 50)
+            this.observer.disconnect();
         }
-      }, 50)
+      });
+      this.observer.observe(this.$el);
+    },
+    destroyed() {
+      this.observer.disconnect();
     }
-    
 }
 </script>
